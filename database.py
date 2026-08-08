65f3be2b6b3f054f8d6d5e0a45fd437bc72f8d78
akiSeriza <akiseriza21@gmail.com>
2026-08-08 08:05:01 +0800

    Add luck modifiers and daily reset functionality to fortunes and economy systems

--- database.py
+++ database.py
@@ -36,62 +36,6 @@ class Database:
         )
         new_balance = (await self.fetchone("SELECT Balance FROM econ WHERE UserID = ?",(userID,)))[0]
         await self.write("INSERT INTO balancehistory (UserID, BalanceChange, BalanceAfter,Timestamp,Reason) VALUES (?,?,?,?,?)",(userID,value,new_balance,time,reason))
-
-    async def bank_deposit(self, userID, deposit, currenttime, bank, isRob):
-        current_bal = await self.fetchone("SELECT Balance FROM econ WHERE UserID = ?", (userID,))
-        current_bal = current_bal[0] if current_bal else 0
-        current_bank_balance = await self.fetchone(
-            """
-            SELECT ba.Balance, ba.LastDepositTime, b.MinimumDepositTime, b.MinimumDeposit
-            FROM banks b
-            LEFT JOIN bankaccounts ba ON b.ShortName = ba.BankType AND ba.UserID = ?
-            WHERE b.ShortName = ?
-            """, 
-            (userID, bank)
-        )
-        print(f"Bank {bank} has recieved request")
-        if current_bank_balance is None:
-            return aiosqlite.Error("Bank not found.")
-        
-        balance = current_bank_balance[0] if current_bank_balance[0] is not None else 0
-        last_deposit = current_bank_balance[1] if current_bank_balance[1] is not None else 0
-        min_cooldown = current_bank_balance[2] if current_bank_balance[2] is not None else 0
-        min_deposit = current_bank_balance[3] if current_bank_balance[3] is not None else 0
-
-        if deposit < 0 and (currenttime - last_deposit < min_cooldown) and isRob == 0:
-            return aiosqlite.Error("Withdrawal cooldown active. Please wait before making another withdrawal.")
-        
-        if min_deposit > deposit > 0 :
-            return aiosqlite.Error(f"Deposit amount is below the minimum allowed (${min_deposit}).")
-        
-        if balance + deposit < 0 or (current_bal < deposit  and isRob == 0): 
-            return aiosqlite.Error("Insufficient funds for this transaction.")
-        log_msg = f"Deposited to bank {bank}" if deposit > 0 else f"Withdrew from bank {bank}"
-
-        if isRob == 0:
-            await self.change_balance(userID, -deposit, log_msg, currenttime)
-        
-        if deposit < 0:
-            print("Withdrawal")
-            await self.write(
-                "UPDATE bankaccounts SET Balance = Balance + ? WHERE UserID = ? AND BankType = ?", 
-                (deposit, userID, bank)
-            )
-        else:
-            print("Deposit")
-            await self.write(
-                """
-                INSERT INTO bankaccounts (UserID, BankType, Balance, LastDepositTime) 
-                VALUES (?, ?, ?, ?) 
-                ON CONFLICT(UserID, BankType) DO UPDATE SET 
-                    Balance = Balance + excluded.Balance,
-                    LastDepositTime = excluded.LastDepositTime
-                """, 
-                (userID, bank, deposit, currenttime)
-            )
-            
-
-
-
+         
     async def close(self):
         await self.conn.close()
--- fortune.py
+++ fortune.py
@@ -44,6 +44,8 @@ class Fortunes(commands.Cog):
         self.refresh.start()
         self.lastrefresh = datetime.date.today()
         self.db: Database = bot.db
+        self.resetluckmod.start()
+        self.dailyluckmod = 0
         self.forced = []
         with open(FORTUNES, mode="r", encoding="utf-8") as f:
             self.fortunes = json.load(f)
@@ -54,12 +56,17 @@ class Fortunes(commands.Cog):
         with open(RAREFORTUNES, mode="r", encoding="utf-8") as f:
             self.rarefortunes = json.load(f)
 
+    @tasks.loop(hours = 24)
+    async def resetluckmod(self):
+        self.dailyluckmod = random.randint(-2,2)
+        await self.db.write("UPDATE econ SET Luck = ?", (self.dailyluckmod,))
+        
 
     @app_commands.command(name= "fortune",description="Draw today's daily fortune!")
     async def fortune(self, interaction: discord.Interaction):
         print(self.forced)
         chance = random.randint(1,50)
-
+        #----------------HardCoded------------------------#
         if chance == 1:
             embed = discord.Embed(
                 title="Today's Fortune",
@@ -74,7 +81,6 @@ class Fortunes(commands.Cog):
 
         if interaction.user.id not in self.accounts:
             chance = random.randint(1,1000)
-            print(f"{await self.bot.fetch_user(interaction.user.id)} drawed {chance}")
             if chance != 1:
                 fortunetitle = random.choice(list(self.fortunes.keys()))
                 fortune = self.fortunes[fortunetitle]
@@ -83,43 +89,64 @@ class Fortunes(commands.Cog):
                 self.accounts[interaction.user.id] = fortunetitle
                 await self.db.change_balance(interaction.user.id, fortune["balChange"],"Fortune", int(time.time()))
                 balChange = f"Your account has been credited {fortune['balChange']} Ichicoins for this fortune"
+                randluckmod = random.randint(-2,2)
+                await self.db.write("UPDATE econ SET LUCK = LUCK + ? + ? WHERE UserID = ?", (randluckmod,fortune["luck"],interaction.user.id))
             else: 
                 fortunetitle = random.choice(list(self.rarefortunes.keys()))
                 fortune = self.rarefortunes[fortunetitle]
                 self.accounts[interaction.user.id] = fortunetitle
                 await self.db.change_balance(interaction.user.id, fortune["balChange"],"Fortune", int(time.time()))
                 balChange = f"Your account has been credited {fortune['balChange']} Ichicoins for this rare fortune"
+                await self.db.write("UPDATE econ SET LUCK = ? WHERE UserID = ?", (fortune["luck"], interaction.user.id))
         else:
-            print("Got here")
             if self.accounts[interaction.user.id] == "comeheremafuyuchan":
                 embed = discord.Embed(
                     title="Today's Fortune",
                     description="**Lucky character:** Akito Shinonome",
                     color=discord.Color.from_str("#FF7722")
                 )
-                print("Got here")
                 embed.add_field(value=thing, name="Quote")
-                print("Got here")
                 embed.set_image(url="https://static.wikitide.net/projectsekaiwiki/thumb/2/2a/Akito_1_art.png/1920px-Akito_1_art.png")
                 embed.set_footer(text ="Your brain has been credited 100 mental damage for calling Mafuyu over.")
-                print("Got here")
                 await interaction.response.send_message(embed=embed)
-                print("Got here")
                 return 
             fortunetitle = self.accounts[interaction.user.id]
-            fortune = self.rarefortunes[fortunetitle] if fortunetitle == "goldenichi" or fortunetitle == "feedme" else self.fortunes[fortunetitle]
+                            
+            if fortunetitle == "goldenichi" or fortunetitle == "feedme":
+                fortune = self.rarefortunes[fortunetitle]
+                await self.db.write("UPDATE econ SET Luck = ? WHERE UserID = ?", (fortune["luck"],interaction.user.id))
+
+            else:
+                fortune = self.fortunes[fortunetitle]
+                randluckmod = random.randint(-2,2)
+                await self.db.write("UPDATE econ SET Luck= Luck + ? + ? WHERE UserID = ?", (randluckmod,fortune["luck"],interaction.user.id))
+                
             balChange = "You have already drawn your fortune for today. Come back later!"
-            print(fortunetitle)
-            print(fortunetitle == "mafufuckyou")
-            print(fortune)
             if interaction.user.id in self.forced:
                 if fortunetitle == "mafufuckyou":
                     fortune["balChange"] = -random.randint(1,10000)
                 self.forced.remove(interaction.user.id)
                 await self.db.change_balance(interaction.user.id, fortune["balChange"],"Fortune", int(time.time()))
                 balChange = f"Your account has been credited {fortune['balChange']} Ichicoins for this fortune"
+                randluckmod = random.randint(-2,2)
 
-        print("Hi")
+        print("Here")
+        luck = await self.db.fetchone("SELECT Luck FROM econ WHERE UserID = ?", (interaction.user.id,))
+        luck  = luck[0] if luck else 0
+        if luck <= -7:
+            note = "WHY IS TH SKY CKACKING— Oh you pulled a Reimu. Ok. Thanks for ending the world."
+        elif luck <= -5:
+            note = "Are you alright over there? Your inner self... all I can see is emptiness in your future..."
+        elif luck == -2:
+            note = "Your life is still down, but it doesnt seem to be as bad as it could be. Take it in stride and follow on."
+        elif luck <= 2:
+            note = "The world is your canvas, lacking of any disturbances from chance; take your brush and paint it! Unless you're Honami. Then dont."
+        elif luck <= 5:
+            note = "A tad bit luckier than usual I see. Maybe try your hand at a few things you usually would not?"
+        elif luck < 7:
+            note = "It's... glowing... you are made for luck! Go ahead and run into the blue!" 
+        else:
+            note = "The one blessed by the Golden hand of Ichika Herself has arrived upon this plane."
         embed = discord.Embed(
             title="Today's Fortune",
             description="**Lucky character:** %s"%fortune["character"],
@@ -127,6 +154,7 @@ class Fortunes(commands.Cog):
         )
         embed.set_thumbnail(url = fortune["thumbnailurl"])
         embed.add_field(name="Quote", value=fortune["entry"])
+        embed.add_field(name="Luck Reading", value=note)
         embed.set_footer(text = balChange)
         print(self.accounts)
         await interaction.response.send_message(embed=embed)
